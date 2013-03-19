b5c65cd2a7c17ea2f30a0b2c89d6b2cdb6bf4783
Iļja Gubins <ilja.gubin@gmail.com>
2013-03-19 17:21:56 +0200

    Login, logout works. Cookies/LocalStorage used to save session.
    Server details - decided to use default Zabbix graphs, however played a
    little bit with using Highcharts for graphing.

--- js/controllers_highcharts.js
+++ js/controllers_highcharts.js
@@ -285,35 +285,324 @@ function serversDetailsController($rootScope, $scope, $http, $routeParams) {
   //TODO
 
   if ($rootScope.loggedIn) {
-    //redo UI, add all graphs on one page, make views for all servers unified
-    $http.post(api_url,
-    {
-      jsonrpc: "2.0",
-      id: $rootScope.auth_id,
-      auth: $rootScope.auth,
-      method: 'host.get',
-      params: {
-        hostids: $routeParams.serverId,
-        //selectTriggers: 'extend',
-        selectGraphs: ['graphid', 'name']
-      }
-    }).success(function (data) {
-      $scope.hostData = data.result;
-      $scope.noOfPages = data.result[0].graphs.length;
-      $scope.currentPage = 1;
-      $scope.currentZoom = 3600;
-      $scope.zoom = 3600;
-      console.log(data);
+
+//*MASTER CHART*//
+  
+    var currentZoom;
+
+    var master_chart = new Highcharts.Chart({
+      chart: {
+        renderTo: 'masterTimeline',
+        reflow: true,
+        borderWidth: 0,
+        backgroundColor: null,
+        marginLeft: 0,
+        marginRight: 20,
+        zoomType: 'x',
+        events: {
+
+          // listen to the selection event on the master chart to update the
+          // extremes of the detail chart
+          selection: function(event) {
+            var extremesObject = event.xAxis[0],
+              min = extremesObject.min,
+              max = extremesObject.max,
+              //detailData = [],
+              xAxis = this.xAxis[0];
+
+            // reverse engineer the last part of the data
+            // jQuery.each(this.series[0].data, function(i, point) {
+            //   if (point.x > min && point.x < max) {
+            //     detailData.push({
+            //       x: point.x,
+            //       y: point.y
+            //     });
+            //   }
+            // });
+
+            // move the plot bands to reflect the new detail span
+            // xAxis.removePlotBand('mask-before');
+            // xAxis.addPlotBand({
+            //   id: 'mask-before',
+            //   from: Date.UTC(2006, 0, 1),
+            //   to: min,
+            //   color: 'rgba(0, 0, 0, 0.2)'
+            // });
+
+            // xAxis.removePlotBand('mask-after');
+            // xAxis.addPlotBand({
+            //   id: 'mask-after',
+            //   from: max,
+            //   to: new Date(),
+            //   color: 'rgba(0, 0, 0, 0.2)'
+            // });
+
+            return false;
+          }
+        }
+      },
+      title: {
+        text: null
+      },
+      xAxis: {
+        type: 'datetime',
+        showLastTickLabel: true,
+        maxZoom: 14 * 24 * 3600000, // fourteen days
+        // plotBands: [{
+        //   id: 'mask-before',
+        //   from: 0,
+        //   // from: Date.UTC(2006, 0, 1),
+        //   to: Date.UTC(2008, 7, 1),
+        //   color: 'rgba(0, 0, 0, 0.2)'
+        // }],
+        title: {
+          text: null
+        }
+      },
+      // yAxis: {
+      //   gridLineWidth: 0,
+      //   labels: {
+      //     enabled: false
+      //   },
+      //   title: {
+      //     text: null
+      //   },
+      //   min: 0.6,
+      //   showFirstLabel: false
+      // },
+      tooltip: {
+        formatter: function() {
+          return false;
+        }
+      },
+      legend: {
+        enabled: false
+      },
+      credits: {
+        enabled: false
+      },
+      plotOptions: {
+        series: {
+          fillColor: {
+            linearGradient: [0, 0, 0, 70],
+            stops: [
+              [0, '#4572A7'],
+              [1, 'rgba(0,0,0,0)']
+            ]
+          },
+          lineWidth: 1,
+          marker: {
+            enabled: false
+          },
+          animate: false,
+          shadow: false,
+          states: {
+            hover: {
+              lineWidth: 1
+            }
+          },
+          enableMouseTracking: false
+        }
+      },
+
+        series: [{
+            data: [
+                [Date.UTC(2010, 0, 1), 29.9],
+                [Date.UTC(2010, 0, 2), 71.5],
+                [Date.UTC(2010, 0, 3), 106.4],
+                [Date.UTC(2010, 0, 6), 129.2],
+                [Date.UTC(2010, 0, 7), 144.0],
+                [Date.UTC(2010, 0, 8), 176.0]
+             ]
+        }]
     });
 
-    //$scope.imageUrl = 'http://zabbixcm02.internal.corp/zabbix/chart2.php?graphid='+$scope.hostData[0].graphs[$scope.currentPage-1].graphid+'&width=800&height=100&period='+$scope.currentZoom;
-    $scope.setZoom = function (zoom) {
-      $scope.currentZoom = zoom;
-    };
+//*Getting all items of the current server*//
 
-    $scope.setPage = function (pageNo) {
-      $scope.currentPage = pageNo;
-    };
+    var serverItems = {};
+    $http.post(api_url, {
+      jsonrpc: "2.0",
+      id: $rootScope.auth_id,
+      method: 'item.get',
+      auth: $rootScope.auth,
+      params: {
+        hostids: $routeParams.serverId,
+        monitored: true,
+        sortfield: 'name',
+        output: ['itemid', 'name', 'key_']
+      }
+    }).success(function (result) {
+      serverItems = result;
+    });
+
+    if (serverItems) {
+      //*CPU History*//
+      $http.post(api_url,
+      {
+        jsonrpc: "2.0",
+        id: $rootScope.auth_id,
+        method: 'history.get',
+        auth: $rootScope.auth,
+        params: {
+          search: {
+            ''
+          }
+        }
+      }).success(function (data) {
+
+      });
+    }
+
+    // $http.post(api_url, {
+
+    // }).success(function (data) {
+
+    // });
+
+// //*CPU*//
+//     $http.post(api_url, {
+//       jsonrpc: "2.0",
+//       id: $rootScope.auth_id,
+//       method: 'item.get',
+//       params: {
+//         output: ['key_', 'name', 'itemid', 'units'],
+//         hostids: $routeParams.serverId,
+//         monitored: true,
+//         search: {
+//           'key_': 'system.cpu.util'
+//         },
+//         sortfield: 'name'
+//       },
+//       auth: $rootScope.auth
+//     }).success(function(data) {
+//       //get CPU history, it it's callback make graph
+//     });
+
+// //*LOAD*//
+//     $http.post(api_url, {
+//       jsonrpc: "2.0",
+//       id: $rootScope.auth_id,
+//       method: 'item.get',
+//       params: {
+//         output: ['key_', 'name', 'itemid', 'units'],
+//         hostids: $routeParams.serverId,
+//         monitored: true,
+//         search: {
+//           'key_': 'system.cpu.util'
+//         },
+//         sortfield: 'name'
+//       },
+//       auth: $rootScope.auth
+//     }).success(function(data) {
+//       //get CPU history, it it's callback make graph
+//     });
+
+// //*RAM*//
+//     $http.post(api_url, {
+//       jsonrpc: "2.0",
+//       id: $rootScope.auth_id,
+//       method: 'item.get',
+//       params: {
+//         output: ['key_', 'name', 'itemid', 'units'],
+//         hostids: $routeParams.serverId,
+//         monitored: true,
+//         search: {
+//           'key_': 'system.cpu.util'
+//         },
+//         sortfield: 'name'
+//       },
+//       auth: $rootScope.auth
+//     }).success(function(data) {
+//       //get CPU history, it it's callback make graph
+//     });
+
+// //*DiskIO*//
+//     $http.post(api_url, {
+//       jsonrpc: "2.0",
+//       id: $rootScope.auth_id,
+//       method: 'item.get',
+//       params: {
+//         output: ['key_', 'name', 'itemid', 'units'],
+//         hostids: $routeParams.serverId,
+//         monitored: true,
+//         search: {
+//           'key_': 'system.cpu.util'
+//         },
+//         sortfield: 'name'
+//       },
+//       auth: $rootScope.auth
+//     }).success(function(data) {
+//       //get CPU history, it it's callback make graph
+//     });
+
+// //*NetworkIO*//
+//     $http.post(api_url, {
+//       jsonrpc: "2.0",
+//       id: $rootScope.auth_id,
+//       method: 'item.get',
+//       params: {
+//         output: ['key_', 'name', 'itemid', 'units'],
+//         hostids: $routeParams.serverId,
+//         monitored: true,
+//         search: {
+//           'key_': 'system.cpu.util'
+//         },
+//         sortfield: 'name'
+//       },
+//       auth: $rootScope.auth
+//     }).success(function(data) {
+//       //get CPU history, it it's callback make graph
+//     });
+
+// //*General Data*//
+//     $http.post(api_url, {
+//       jsonrpc: "2.0",
+//       id: $rootScope.auth_id,
+//       method: 'item.get',
+//       params: {
+//         output: ['key_', 'name', 'itemid', 'units'],
+//         hostids: $routeParams.serverId,
+//         monitored: true,
+//         search: {
+//           'key_': 'system.cpu.util'
+//         },
+//         sortfield: 'name'
+//       },
+//       auth: $rootScope.auth
+//     }).success(function(data) {
+//       //get CPU history, it it's callback make graph
+//     });
+
+
+    //redo UI, add all graphs on one page, make views for all servers unified
+    // $http.post(api_url,
+    // {
+    //   jsonrpc: "2.0",
+    //   id: $rootScope.auth_id,
+    //   auth: $rootScope.auth,
+    //   method: 'host.get',
+    //   params: {
+    //     hostids: $routeParams.serverId,
+    //     //selectTriggers: 'extend',
+    //     selectGraphs: ['graphid']
+    //   }
+    // }).success(function (data) {
+    //   $scope.hostData = data.result;
+    //   $scope.noOfPages = data.result[0].graphs.length;
+    //   $scope.currentPage = 1;
+    //   $scope.currentZoom = 3600;
+    //   $scope.zoom = 3600;
+    //   console.log(data);
+    // });
+
+    // //$scope.imageUrl = 'http://zabbixcm02.internal.corp/zabbix/chart2.php?graphid='+$scope.hostData[0].graphs[$scope.currentPage-1].graphid+'&width=800&height=100&period='+$scope.currentZoom;
+    // $scope.setZoom = function (zoom) {
+    //   $scope.currentZoom = zoom;
+    // };
+
+    // $scope.setPage = function (pageNo) {
+    //   $scope.currentPage = pageNo;
+    // };
   }
 }
 
